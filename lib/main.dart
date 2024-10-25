2ea2ca6042b423708fdddbfe9aff372bea342b45
Gerard Gutiérrez <ggutiflo@gmail.com>
2024-10-25 18:21:24 +0200

    wip camera view

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'app/views/home_view.dart';
+import 'app/views/home/home_view.dart';
 
 void main() {
   runApp(
